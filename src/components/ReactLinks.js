eb38ec4a366077b361adc9ed274bcafc2e7fbbe5
Chan Tsz Him Oscar <Oscar@Chans-MacBook-Pro-9.local>
2021-04-11 15:52:11 +0800

    done

--- src/components/ReactLinks.js
+++ src/components/ReactLinks.js
@@ -2,20 +2,47 @@ import React, { useState } from 'react';
 import modal from './modal'
 
 const ReactLinks = ({ item, remove }) => {
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const handleChange = event => {
+        setSearchTerm(event.target.value);
+    };
 
-
-
+    const results = !searchTerm
+        ? item
+        : item.filter(a =>
+            a.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+        );
 
     return (
+
+
         <div className="p-2 d-flex justify-content-center">
-            <ol>
-                {item.map((item) =>
-                    <li className="pt-3" key={item.id}>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Search"
+                    value={searchTerm}
+                    onChange={handleChange}
+                />
+
+                {results.map(item => (
+                    <div>
                         <a href={`//${item.url}`}>{item.name}</a>
                         <p>Tags: {item.tags}</p>
-                        <button onClick={() => remove(item.id)} className="btn btn-danger">delete</button>
-                    </li>)}
-            </ol>
+                    </div>
+                ))}
+
+            </div>
+            <div>
+                <ol>
+                    {item.map((item) =>
+                        <li className="pt-3" key={item.id}>
+                            <a href={`//${item.url}`}>{item.name}</a>
+                            <p>Tags: {item.tags}</p>
+                            <button onClick={() => remove(item.id)} className="btn btn-danger">delete</button>
+                        </li>)}
+                </ol>
+            </div>
         </div>
     );
 }
